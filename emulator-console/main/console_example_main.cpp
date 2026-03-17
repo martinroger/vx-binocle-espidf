f5b54519f45413df43cdfa022bd4e138cef20588
Martin ROGER <martin.roger@me.com>
2026-03-17 18:06:42 +0100

    New PWM function added

--- emulator-console/main/console_example_main.cpp
+++ emulator-console/main/console_example_main.cpp
@@ -149,6 +149,7 @@ extern "C" void app_main(void)
     register_setSpeedKPH();
     register_setSpeedMPH();
     register_getSpeed();
+    register_pwm();
 
     register_setExpIO();
     register_printExpStatus();
